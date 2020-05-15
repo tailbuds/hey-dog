6339325f12adec318761ceda181dabbce8130eca
RevanthNemani <raynemani@gmail.com>
2020-05-15 04:20:43 +0400

    Removed all files that are in the .gitignore

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const sequelize = require('./config/database');
 
 const server = http.createServer();
 
-const breedInfo = sequelize.import('breedInfo', require('./models/breedinfo'));
+const breedInfo = sequelize.import('breedInfo', require('./models/breedInfo'));
 const measurementUnit = sequelize.import(
   'measurementUnits',
-  require('./models/measurementunits')
+  require('./models/measurementUnits')
 );
 const countries = sequelize.import('countries', require('./models/countries'));
 const breedImages = sequelize.import('images', require('./models/images'));
