5f1a3412b279fadbc28fa8140f71df71c4940aed
Nemani <43512987+RevanthNemani@users.noreply.github.com>
2020-06-04 11:21:34 +0400

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,22 @@
+//Enviroment Check
+let SECRET_KEY;
+
+if (process.env.NODE_ENV === 'development') {
+  SECRET_KEY = process.env.DEV_SECRET_KEY;
+}
+if (process.env.NODE_ENV === 'test') {
+  SECRET_KEY = process.env.TEST_SECRET_KEY;
+}
+if (process.env.NODE_ENV === 'production') {
+  SECRET_KEY = process.env.PROD_SECRET_KEY;
+}
+
 exports.headCheck = (req, res, next) => {
-  if (
-    req.header('post-api-key') ||
-    req.header('patch-api-key') ||
-    req.header('put-api-key') ||
-    req.header('delete-api-key')
-  ) {
-    if (req.header('post-api-key') === process.env.DEV_SECRET_KEY) {
-      next();
-    }
-
-    if (req.header('patch-api-key') === process.env.DEV_SECRET_KEY) {
-      next();
-    }
-
-    if (req.header('put-api-key') === process.env.DEV_SECRET_KEY) {
-      next();
-    }
-
-    if (req.header('delete-api-key') === process.env.DEV_SECRET_KEY) {
-      next();
-    }
-  } else {
-    res.status(400).json({ error: secretKeyNeeded });
+  if (!req.header('x-api-key')) {
+    return res.status(400).json({ error: secretKeyNeeded });
   }
+  if (req.header('x-api-key') !== SECRET_KEY) {
+    return res.status(404).json({ error: secretKeyWrong });
+  } 
+  next();
 };
